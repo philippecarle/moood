8b64cfb2a64c1681115130e401a92435bf6547bf
Philippe Carle <c@rlephilip.pe>
2020-08-13 21:27:08 -0400

    Rename username to email

--- src/api/internal/models/user.go
+++ src/api/internal/models/user.go
@@ -4,14 +4,14 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 // Credentials is a struct that models the structure of a user login request body
 type Credentials struct {
-	Username string `json:"username"`
+	Email string `json:"email"`
 	Password string `json:"password"`
 }
 
 // User is a struct that models the structure of a user in the DB
 type User struct {
 	ID              primitive.ObjectID `json:"id" bson:"_id,omitempty"`
-	Username        string             `json:"username" bson:"username"`
+	Email        string             `json:"email" bson:"email"`
 	FirstName       string             `json:"firstName" bson:"firstName"`
 	LastName        string             `json:"lastName" bson:"lastName"`
 	EncodedPassword string             `json:"-" bson:"password"`
@@ -20,7 +20,7 @@ type User struct {
 
 // UserRegistration is a struct that models the structure of a user registration request body
 type UserRegistration struct {
-	Username      string `json:"username"`
+	Email      string `json:"email"`
 	FirstName     string `json:"firstName"`
 	LastName      string `json:"lastName"`
 	ClearPassword string `json:"password"`
